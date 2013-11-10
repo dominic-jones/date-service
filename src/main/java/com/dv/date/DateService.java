9ea2454a8550e05c17f2f6baef1fa0a53ef019ab
Dominic Jones <dominic.jones@elasticpath.com>
2013-11-10 09:55:16 +0000

    More explicitly stating rest paths

--- src/main/java/com/dv/date/DateService.java
+++ src/main/java/com/dv/date/DateService.java
@@ -1,24 +1,18 @@
 package com.dv.date;
 
-import com.google.common.base.Function;
 import org.joda.time.DateTime;
 
-import javax.annotation.Nullable;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.Map;
 
-import static com.dv.date.Location.toDateTime;
-import static com.google.common.collect.Iterables.transform;
-import static com.google.common.collect.Maps.uniqueIndex;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.joda.time.DateTimeZone.UTC;
 
 @Stateless
-@Path("dates")
+@Path("/dates")
 public class DateService {
 
     @Inject
@@ -26,23 +20,12 @@ public class DateService {
 
     @GET
     @Produces(APPLICATION_JSON)
-    public Map<String, DateTime> dates() {
+    public DatesVO dates() {
 
         final DateTime sourceDate = new DateTime(UTC)
                 .withDate(2010, 10, 20)
                 .withTime(0, 0, 0, 0);
 
-        Iterable<DateTime> dates = transform(findLocationsQuery.execute(), toDateTime(sourceDate));
-
-        return uniqueIndex(dates, new Function<DateTime, String>() {
-            @Nullable
-            @Override
-            public String apply(@Nullable DateTime dateTime) {
-
-                assert dateTime != null;
-                return dateTime.getZone().getID();
-            }
-        });
+        return new DatesVO(sourceDate, findLocationsQuery.execute());
     }
-
 }
